99c97c4f94f029289d87be3c4057a11b5ff3deed
NathanOnCodes <contatonathancf@gmail.com>
2025-01-14 20:04:12 -0300

    feat: add conversion controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConversionController } from './conversion/conversion.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ConversionController],
   providers: [AppService],
 })
 export class AppModule {}
