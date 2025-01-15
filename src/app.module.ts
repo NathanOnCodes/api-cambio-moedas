a2d0453e744197b303309dc36342ea9cbc9333e7
NathanOnCodes <contatonathancf@gmail.com>
2025-01-15 19:05:51 -0300

    chore: config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConversionController } from './conversion/conversion.controller';
+import { ConversionService } from './conversion/conversion.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres_db',
+      port: 5432,   
+      username: 'adm',
+      password: 'password',
+      database: 'dbpostgres',
+      entities: [`${__dirname}/typeorm/entities/*{.ts,.js}`],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController, ConversionController],
-  providers: [AppService],
+  providers: [AppService, ConversionService],
 })
 export class AppModule {}
