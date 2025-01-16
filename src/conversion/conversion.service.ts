fcc4a05de6babc5c92632705c2fec3b3181fb6a0
NathanOnCodes <contatonathancf@gmail.com>
2025-01-16 20:43:04 -0300

    feat: add new method findall

--- src/conversion/conversion.service.ts
+++ src/conversion/conversion.service.ts
@@ -1,4 +1,16 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Conversion } from 'src/entities/conversion.entity';
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
-export class ConversionService {}
+export class ConversionService {
+    constructor (
+        @InjectRepository(Conversion) private conversionRepository: Repository<Conversion>
+      ){}
+
+    async findAllExchanges(): Promise<Conversion[]>{
+        return this.conversionRepository.find();
+    }
+}
