5f425ed76c1856fda993a2e2cbd2b55eef29baf1
NathanOnCodes <contatonathancf@gmail.com>
2025-01-16 20:40:49 -0300

    feat: add new endpoint get method

--- src/conversion/conversion.controller.ts
+++ src/conversion/conversion.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ConversionService } from './conversion.service';
+import { Conversion } from 'src/entities/conversion.entity';
 
-@Controller('conversion')
-export class ConversionController {}
+@Controller('api/v1/exchange/')
+export class ConversionController {
+    constructor(
+        private readonly conversionService: ConversionService
+    ){}
+
+    @Get()
+    findExchanges(): Promise<Conversion[]>{
+        return this.conversionService.findAllExchanges()
+    }
+
+}
