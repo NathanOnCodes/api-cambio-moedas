29817928e588e9995e3dec6658ec936f8afe1433
NathanOnCodes <contatonathancf@gmail.com>
2025-01-17 21:50:48 -0300

    feat: add new createConversion endpoint

--- src/conversion/conversion.controller.ts
+++ src/conversion/conversion.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { ConversionService } from './conversion.service';
 import { Conversion } from 'src/entities/conversion.entity';
+import { CreateConversionDto } from './dto/create-conversion.dto';
 
 @Controller('api/v1/exchange/')
 export class ConversionController {
@@ -10,7 +11,14 @@ export class ConversionController {
 
     @Get()
     findExchanges(): Promise<Conversion[]>{
-        return this.conversionService.findAllExchanges()
+        return this.conversionService.findAllExchanges();
+    }
+
+    @Post(':amount/:from//:to/:rate/')
+    createConversion(
+        @Param() params: CreateConversionDto
+    ): Promise<{conversion: Conversion, status: HttpStatus}>{
+        return this.conversionService.newConversion(params);
     }
 
 }
