906dfbc8a9c56244cbe5e486798ab98bc58fd79f
NathanOnCodes <contatonathancf@gmail.com>
2025-01-22 22:56:16 -0300

    feat: delete endpoint refactor

--- src/conversion/conversion.controller.ts
+++ src/conversion/conversion.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { ConversionService } from './conversion.service';
 import { Conversion } from 'src/entities/conversion.entity';
 import { CreateConversionDto } from './dto/create-conversion.dto';
@@ -14,11 +14,18 @@ export class ConversionController {
         return this.conversionService.findAllExchanges();
     }
 
-    @Post(':amount/:from/:to/:rate/')
+    @Post(':amount/:from/:to/:rate')
     createConversion(
         @Param() params: CreateConversionDto
     ): Promise<{conversion: Conversion, status: HttpStatus}>{
         return this.conversionService.newConversion(params);
     }
 
+    @Delete('delete/:id')
+    deleteConversion(
+        @Param('id') id: number
+    ): HttpStatus {
+        this.conversionService.destroy(id);
+        return HttpStatus.OK;
+    }
 }
