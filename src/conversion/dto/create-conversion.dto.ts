07ff26ae107a61569e58815c588715ed854caa61
NathanOnCodes <contatonathancf@gmail.com>
2025-01-23 16:49:06 -0300

    feat: add swagger api decorator

--- src/conversion/dto/create-conversion.dto.ts
+++ src/conversion/dto/create-conversion.dto.ts
@@ -1,17 +1,23 @@
 import { IsNumber, IsString, Min, IsDecimal } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateConversionDto {
+
+    @ApiProperty({ type: Number })
     @IsNumber()
     @Min(0)
     @IsDecimal({ decimal_digits: "2" })
     readonly amount: number;
 
+    @ApiProperty({ type: String })
     @IsString()
     readonly from: string;
 
+    @ApiProperty({ type: String })
     @IsString()
     readonly to: string;
 
+    @ApiProperty({ type: Number })
     @IsNumber()
     @Min(0)
     @IsDecimal({ decimal_digits: "2" })
