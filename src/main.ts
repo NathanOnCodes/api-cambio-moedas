aebc4e03f3378435afd1227e787aeae6565d2427
NathanOnCodes <contatonathancf@gmail.com>
2025-02-04 15:58:57 -0300

    chore: refactor swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { swagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('API de cambio')
-    .setDescription('Converta moedas com base na cotação do dia.')
-    .setVersion('1.0')
-    .build();
-
-  const documentFactory = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  swagger(app);
+  await app.listen(3000);
 }
 bootstrap();
